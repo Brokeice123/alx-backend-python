bb86f71e05cb3c24fc2581b93515390a4a5f9ac5
Brokeice123 <keithmarkomen@gmail.com>
2024-05-02 23:16:52 +0300

    Formatting

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,7 +6,7 @@ from typing import Sequence, Any, Union
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
   """Return the first element of a list if it exists, otherwise None."""
-    if lst:
-        return lst[0]
-    else:
-        return None
+  if lst:
+      return lst[0]
+  else:
+      return None
