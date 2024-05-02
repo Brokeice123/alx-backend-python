72ec146defb9d026079380c7a3c460a736b50318
Brokeice123 <keithmarkomen@gmail.com>
2024-05-02 23:15:57 +0300

    Changes

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """This module defines the safe_first_element function."""
 
-from typing import Sequence, Any, Union, Type
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, Type[None]]:
-    """Return the first element of a list."""
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+  """Return the first element of a list if it exists, otherwise None."""
     if lst:
         return lst[0]
     else:
