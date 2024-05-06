036c7c0d5113c476032fad07b69b173b01777397
Brokeice123 <keithmarkomen@gmail.com>
2024-05-06 15:08:54 +0300

    Import

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ This module is an example of how to use asyncio to run concurrent coroutines.
 
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+from 0-basic_async_syntax import wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
