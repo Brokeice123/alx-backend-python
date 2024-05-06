b85ead276b8d1cbad4830b10bdb89e430ff69ca8
Brokeice123 <keithmarkomen@gmail.com>
2024-05-06 16:13:54 +0300

    Output error

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,4 +12,6 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     task_wait_n function
     """
-    return [await task_wait_random(max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delay = [await task for task in asyncio.as_completed(tasks)]
+    return delay
