ab3309daf94533836a46082e390ee0f3426d2eb3
Brokeice123 <keithmarkomen@gmail.com>
2024-05-06 16:10:42 +0300

    4. Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python module that contains the task_wait_random function
+"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    task_wait_n function
+    """
+    return [await task_wait_random(max_delay) for _ in range(n)]
