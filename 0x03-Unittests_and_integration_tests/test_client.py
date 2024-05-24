f5eb175fbe1cbe0aa08151844107e1e802a41386
Brokeice123 <keithmarkomen@gmail.com>
2024-05-24 23:54:51 +0300

    Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -86,3 +86,37 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("google")
 
         self.assertEqual(client.has_license(repo, key), expected)
+
+
+@parameterized_class([
+    {
+        'org_payload': TEST_PAYLOAD[0][0],
+        'repos_payload': TEST_PAYLOAD[0][1],
+        'expected_repos': TEST_PAYLOAD[0][2],
+        'apache2_repos': TEST_PAYLOAD[0][3],
+    },
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration test for GitHub organization client"""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Setup class"""
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            """Get the payload for a given URL"""
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Teardown class"""
+        cls.get_patched.stop()
